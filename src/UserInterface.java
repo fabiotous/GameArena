5beb5cd22c6ff6deed5282625d8124b0d78d0191
Fabio Tous <fabio_zinquer@hotmail.com>
2023-04-04 22:25:55 -0400

    Update UserInterface.java

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -7,18 +7,19 @@ public class UserInterface extends JFrame implements ActionListener {
     private JTextField searchField;
     private JTextArea resultsArea;
 
-    public UserInterface(ArrayList<User> users) {
+    private User curuser;
+
+    public UserInterface(User curuser, ArrayList<User> users) {
         super("Find a friend");
+        this.curuser = curuser;
         this.users = users;
 
-        // Create components
         JLabel searchLabel = new JLabel("Search:");
         searchField = new JTextField(20);
         JButton searchButton = new JButton("Search");
         resultsArea = new JTextArea(10, 30);
         resultsArea.setEditable(false);
 
-        // Add components to frame
         JPanel mainPanel = new JPanel(new BorderLayout());
         JPanel searchPanel = new JPanel(new FlowLayout());
         searchPanel.add(searchLabel);
@@ -28,10 +29,8 @@ public class UserInterface extends JFrame implements ActionListener {
         mainPanel.add(new JScrollPane(resultsArea), BorderLayout.CENTER);
         setContentPane(mainPanel);
 
-        // Add action listener to search button
         searchButton.addActionListener(this);
 
-        // Set frame properties
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         pack();
         setVisible(true);
@@ -41,20 +40,45 @@ public class UserInterface extends JFrame implements ActionListener {
         String searchText = searchField.getText().toLowerCase();
         resultsArea.setText("");
 
+        JPopupMenu popupMenu = new JPopupMenu();
+        JMenuItem inviteMenuItem = new JMenuItem("Invite to Tournament Lobby");
+        popupMenu.add(inviteMenuItem);
+
         for (User user : users) {
             if (user.getName().toLowerCase().contains(searchText)) {
-                resultsArea.append(user.print() + "\n");
+                String userInformation = user.print();
+                resultsArea.append(userInformation + "\n");
+
+                resultsArea.addMouseListener(new MouseAdapter() {
+                    public void mouseEntered(MouseEvent e) {
+                        resultsArea.setToolTipText(userInformation);
+                    }
+                    public void mouseExited(MouseEvent e) {
+                        resultsArea.setToolTipText(null);
+                    }
+                    public void mousePressed(MouseEvent e) {
+                        if (SwingUtilities.isRightMouseButton(e)) {
+                            popupMenu.show(e.getComponent(), e.getX(), e.getY());
+                        }
+                    }
+                });
+
+                inviteMenuItem.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        TournamentGUI lobby = new TournamentGUI(curuser.getName(), user.getName());
+                    }
+                });
             }
         }
     }
-//    public static void main(String[] args) {
-//        // Sample data
-//        ArrayList<User> users = new ArrayList<User>();
-//        users.add(new User("Peter09", "peter@gmail.ca", "Peter"));
-//        users.add(new User("John_Sol", "jsol@gmail.ca", "John"));
+
+   // public static void main(String[] args) {
+    //   ArrayList<User> users = new ArrayList<User>();
+    //    User curuser = new User("KyleP", "kylep@gmail.ca", "Kyle");
+    //    users.add(new User("Peter09", "peter@gmail.ca", "Peter"));
+    //    users.add(new User("John_Sol", "jsol@gmail.ca", "John"));
 //
-//        // Create GUI
-//        UserInterface gui = new UserInterface(users);
-//    }
+     //   UserInterface gui = new UserInterface(curuser ,users);
+    //}
 
 }
