5686513e465a9a50a7cb60080660665c0cc63453
Anish Ahuja <anishahuja46@gmail.com>
2023-04-10 22:52:29 -0400

    UserAccount.java now reads from database.txt for user data

--- src/UserAccount.java
+++ src/UserAccount.java
@@ -3,6 +3,9 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class UserAccount extends JFrame {
     private JPanel panel;
@@ -18,10 +21,37 @@ public class UserAccount extends JFrame {
         this.username = username;
         greeting.setText("Welcome " + username + "!");
 
-        // Replace 0 with wins/losses/ties from somewhere
-        stats.append("Wins: " + 0 + "\n");
-        stats.append("Losses: " + 0 + "\n");
-        stats.append("Ties: " + 0 + "\n");
+        // Code below is for reading from txt file and printing it.
+        // this is used to count number of lines each user has in terms of game data
+        int count = 0;
+
+        String fileName = "database.txt"; // Update with the correct file name
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            boolean found = false;
+            while ((line = br.readLine()) != null) {
+
+                //right now every user has 24 lines, but this number can be changed if more games are added or data information is expanded
+                if (count == 24){
+                    break;
+                } else if (line.contains("Password") && line.contains(username)) {
+                    found = true;
+                    stats.append("Player Data: \n");
+                } else if (found) {
+                    stats.append(line + "\n");
+                    count += 1;
+                }
+            }
+
+            if (!found) {
+                System.out.println("Player data not found for username: " + username);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //////////////////////////////////////////////////////////////////////////////
+
         stats.setVisible(false);
 
         setContentPane(panel);
