7f681964a255ec6caab67f72970d0f00e34dc272
Fabio Tous <fabio_zinquer@hotmail.com>
2023-04-11 00:25:13 -0400

    Update UserInterface.java

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,18 +1,20 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 import javax.swing.*;
 public class UserInterface extends JFrame implements ActionListener {
-    private ArrayList<User> users;
+    private String username;
     private JTextField searchField;
     private JTextArea resultsArea;
 
     private User curuser;
 
-    public UserInterface(User curuser, ArrayList<User> users) {
+    public UserInterface(String username) {
         super("Find a friend");
-        this.curuser = curuser;
-        this.users = users;
+        this.username = username;
 
         JLabel searchLabel = new JLabel("Search:");
         searchField = new JTextField(20);
@@ -37,48 +39,67 @@ public class UserInterface extends JFrame implements ActionListener {
     }
 
     public void actionPerformed(ActionEvent e) {
-        String searchText = searchField.getText().toLowerCase();
+        String searchText = searchField.getText();
         resultsArea.setText("");
 
         JPopupMenu popupMenu = new JPopupMenu();
         JMenuItem inviteMenuItem = new JMenuItem("Invite to Tournament Lobby");
         popupMenu.add(inviteMenuItem);
 
-        for (User user : users) {
-            if (user.getName().toLowerCase().contains(searchText)) {
-                String userInformation = user.print();
-                resultsArea.append(userInformation + "\n");
+        try {
+            File file = new File("database.txt");
+            Scanner scanner = new Scanner(file);
+            Boolean usernamefound = false;
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String targetusername = "";
+                if (line.length() >= 10) {
+                    if (line.substring(0, 9).equals("Username:")) {
+                        targetusername = line.substring(10);
+                    }
+                }
+                if (searchText.equals(targetusername)) {
+                    usernamefound = true;
+                    if (searchText.equals(targetusername)) {
+                        //String userInformation = user.print();
+                        resultsArea.append(searchText + "\n");
 
-                resultsArea.addMouseListener(new MouseAdapter() {
-                    public void mouseEntered(MouseEvent e) {
-                        resultsArea.setToolTipText(userInformation);
-                    }
-                    public void mouseExited(MouseEvent e) {
-                        resultsArea.setToolTipText(null);
-                    }
-                    public void mousePressed(MouseEvent e) {
-                        if (SwingUtilities.isRightMouseButton(e)) {
-                            popupMenu.show(e.getComponent(), e.getX(), e.getY());
-                        }
-                    }
-                });
+                        resultsArea.addMouseListener(new MouseAdapter() {
+                            public void mouseEntered(MouseEvent e) {
+                                resultsArea.setToolTipText(searchText);
+                            }
 
-                inviteMenuItem.addActionListener(new ActionListener() {
-                    public void actionPerformed(ActionEvent e) {
-                        TournamentGUI lobby = new TournamentGUI(curuser.getName(), user.getName());
+                            public void mouseExited(MouseEvent e) {
+                                resultsArea.setToolTipText(null);
+                            }
+
+                            public void mousePressed(MouseEvent e) {
+                                if (SwingUtilities.isRightMouseButton(e)) {
+                                    popupMenu.show(e.getComponent(), e.getX(), e.getY());
+                                }
+                            }
+                        });
+
+                        inviteMenuItem.addActionListener(new ActionListener() {
+                            public void actionPerformed(ActionEvent e) {
+                                TournamentGUI lobby = new TournamentGUI(username, searchText);
+                            }
+                        });
                     }
-                });
+                }
             }
+            if (usernamefound == false) {
+                JOptionPane.showMessageDialog(null, "Invalid username provided, please try again.", "User not found", JOptionPane.ERROR_MESSAGE);
+            }
+            scanner.close();
+        } catch (FileNotFoundException f) {
+            System.out.println("File not found.");
         }
+
     }
 
    // public static void main(String[] args) {
-    //   ArrayList<User> users = new ArrayList<User>();
-    //    User curuser = new User("KyleP", "kylep@gmail.ca", "Kyle");
-    //    users.add(new User("Peter09", "peter@gmail.ca", "Peter"));
-    //    users.add(new User("John_Sol", "jsol@gmail.ca", "John"));
-//
-     //   UserInterface gui = new UserInterface(curuser ,users);
-    //}
+   //     UserInterface gui = new UserInterface("Bob");
+   // }
 
 }
